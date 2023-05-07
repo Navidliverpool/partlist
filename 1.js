fc442b9fedd3908ae3b10f357908a901495a6fcd
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-05-07 19:19:24 +0100

    done

--- 1.js
+++ 1.js
@@ -1,3 +1,15 @@
 function partlist(arr) {
-
+    let a = ""
+    let rr = arr
+    let n = arr.length
+    let result = []
+    for (let i = 0; i < n - 1; i++) {
+        a += ' ' + arr[i]
+        rr = rr.slice(1)
+        let r = []
+        r.push(a.trim(), rr.join(' '))
+        result.push(r)
+    }
+    return result
 }
+console.log(partlist(["I", "wish", "I", "hadn't", "come"]))
